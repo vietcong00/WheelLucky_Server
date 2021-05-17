b871191dd5e25c32c148916667ea155b3eec9b37
DESKTOP-FK6K6LB\tient <tientung1412@gmail.com>
2021-05-17 09:27:37 +0700

    create server

---
+++ src/main/java/project3/backend/helper/helper/JwtTokenUtil.java
@@ -0,0 +1,43 @@
+package project3.backend.helper.helper;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.lang3.RandomStringUtils;
+import project3.backend.model.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JwtTokenUtil {
+
+    private String secret = RandomStringUtils.randomAlphanumeric(10);
+
+    //generate token for player
+    public String generateToken(Player player) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, player.getUsernamePlayer());
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+    public String getToken(List<String> cookie) {
+        String token = null;
+        for (String s : cookie) {
+            if (s.indexOf("token=") >= 0) {
+                int start = s.indexOf("token=");
+                if(s.indexOf(';',start)>0){
+                    token = s.substring(start,start+(s.indexOf(';',start)));
+                }else {
+                    token = s.substring(start + 6);
+                }
+                break;
+            }
+        }
+        return token;
+    }
+
+}
